b0500dcfe0ca39447ee2c69a3a7693df9c9d5597
harderoder <harderoder@gmail.com>
2013-11-21 18:38:12 +0300

    Update and rename posmotri to kolvo_vh_sym.pl

---
+++ kolvo_vh_sym.pl
@@ -0,0 +1,30 @@
+
+    
+#!/usr/bin/perl
+  use strict;
+  use warnings;
+  use Getopt::Long;
+  
+my $names="";
+GetOptions ("name=s" => \$names );
+open FILE, "<", $names or die $!;
+  undef(local $/);
+  my $text = <FILE>;
+  
+close FILE;
+
+my @list = split(//, $text);
+my %h;
+
+
+$h{$_}++ foreach (@list);
+
+
+foreach (keys %h) {
+          $_=$h{$_};
+}
+     
+     
+          foreach(sort {$h{$b} <=> $h{$a}} keys %h) {
+          print $_,'=',$h{$_},"\n";
+}
